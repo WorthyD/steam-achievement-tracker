c1de3670b691e00c76e357c31b7a4119c01f1c2d
Daniel Worthy <daniel.worthy@rockfishinteractive.com>
2018-02-07 20:37:34 -0600

    Add project files.

---
+++ SteamAchievementTrackerNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SteamAchievementTrackerNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
