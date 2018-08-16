c8198e0001e165918f6fa895095e8da447563f4d
Daniel Worthy <daniel.worthy@vml.com>
2018-08-15 21:32:39 -0500

    Making adjustments currently a broken build

--- tests/sat-tests-shared/TestFixture.cs
+++ tests/sat-tests-shared/TestFixture.cs
@@ -1,8 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Security.Claims;
 using System.Text;
+using System.Threading.Tasks;
+
 using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
 using sat_dal;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 
 namespace sat_tests_shared
 {
@@ -17,6 +30,21 @@ namespace sat_tests_shared
                   configure => { configure.AddProfile<DalMappingProfile>(); }
               )
           );
+        }
+
+        private static HttpClient GetTestServer()
+        {
+            var server = new TestServer(
+                new WebHostBuilder()
+                    .UseStartup<Startup>()
+                    .ConfigureAppConfiguration(config =>
+                    {
+                        config.SetBasePath(Path.Combine(Path.GetFullPath(@"../../../.."), "Kodkod.Tests.Shared"));
+                        config.AddJsonFile("appsettings.json", false);
+                    })
+            );
+
+            return server.CreateClient();
         }
     }
 }
