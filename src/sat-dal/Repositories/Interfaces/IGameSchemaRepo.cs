7324fe5520d5478a1706f2ec6ef567f5144ca590
Daniel Worthy <danieljworthy@gmail.com>
2018-08-17 06:54:34 -0500

    updates

---
+++ src/sat-dal/Repositories/Interfaces/IGameSchemaRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sat_dal.Repositories
+{
+    public interface IGameSchemaRepo
+    {
+        Task<Models.GameSchema> Load(object appId);
+        Task<Models.GameSchema> SaveSchema(long AppId, sat_contracts.models.ServiceModels.IGame game, sat_contracts.models.ServiceModels.IAchievementPercentages serviceAch);
+    }
+}
