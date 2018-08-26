689dffee5430b3285895b238c249aa429146f0c8
Daniel Worthy <danieljworthy@gmail.com>
2018-08-26 06:22:05 -0500

    Updates

---
+++ src/sat-business/BusinessMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using sat_business.DTOs;
+
+namespace sat_business
+{
+    public class BusinessMappingProfile : Profile
+    {
+        public BusinessMappingProfile()
+        {
+            CreateMap<SteamApiWrapper.Models.PlayerSummaries.Player, PlayerProfileDTO>()
+                .ForMember(dest => dest.AvatarFull, opt => opt.MapFrom(src => src.avatarfull))
+                .ForMember(dest => dest.PersonaName, opt => opt.MapFrom(src => src.personaname))
+                .ForMember(dest => dest.ProfileUrl, opt => opt.MapFrom(src => src.profileurl))
+                .ForMember(dest => dest.RealName, opt => opt.MapFrom(src => src.profileurl ?? "Not Found"))
+                .ForMember(dest => dest.SteamId, opt => opt.MapFrom(src => long.Parse(src.steamid)));
+
+        }
+    }
+}
--- src/sat-business/Providers/PlayerProfileProvider.cs
+++ src/sat-business/Providers/PlayerProfileProvider.cs
@@ -6,6 +6,9 @@ using sat_dal.Models;
 using sat_contracts.repositories;
 using sat_contracts.models;
 using sat_dal.Repositories;
+using System.Linq;
+using AutoMapper;
+using sat_business.DTOs;
 
 namespace sat_business.Providers
 {
@@ -14,10 +17,13 @@ namespace sat_business.Providers
 
         private readonly IPlayerProfileRepo _profileRepo;
 
-        public PlayerProfileProvider(IPlayerProfileRepo _repo)
+        private readonly IMapper _mapper;
+
+        public PlayerProfileProvider(IPlayerProfileRepo _repo, IMapper mapper)
            : base("AE24AB02B6610D51BA9C8EA4128D11F3")
         {
             this._profileRepo = _repo;
+            this._mapper = mapper;
 
         }
 
@@ -28,7 +34,11 @@ namespace sat_business.Providers
 
             var profile = await this._profileRepo.Load(steamID);
 
-            if (profile == null || ExperationService.isProfileExpired(profile.LastUpdate))
+            if (profile == null)
+            {
+
+            }
+            else if (ExperationService.isProfileExpired(profile.LastUpdate))
             {
 
             }
@@ -36,6 +46,41 @@ namespace sat_business.Providers
 
 
             return null;
+        }
+        private async Task<IPlayerProfile> AddUserProfile(long steamId)
+        {
+            var steamProfile = this.GetExternalProfile(steamId);
+            PlayerProfileDTO playerProfile = this._mapper.Map<PlayerProfileDTO>(steamProfile);
+            playerProfile.LastUpdate = DateTime.Now;
+            playerProfile.LibraryLastUpdate = new DateTime(2000, 1, 1);
+
+            await this._profileRepo.Save(steamId, playerProfile);
+            return playerProfile;
+        }
+
+        private async Task<IPlayerProfile> UpdateUserProfile(PlayerProfile profile)
+        {
+
+            return null;
+        }
+
+
+
+        private async Task<SteamApiWrapper.Models.PlayerSummaries.Player> GetExternalProfile(long steamId)
+        {
+
+            var request = new SteamApiWrapper.SteamUser.GetPlayerSummariesRequest(base.APIKey);
+            request.ProfileIds = new List<long>();
+            request.ProfileIds.Add(steamId);
+
+            var response = await request.GetResponse();
+
+            if (response == null || response.Players == null)
+            {
+                //Throw new exception
+            }
+            return response.Players.FirstOrDefault();
+
         }
 
     }
