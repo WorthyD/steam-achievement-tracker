ff981dec283dc1d3f1bb2d195f4da55a8c5d428c
Daniel Worthy <danieljworthy@gmail.com>
2018-09-21 06:10:48 -0500

    updating with fresh ng install

--- src/sat-client-app/src/app/app.component.spec.ts
+++ src/sat-client-app/src/app/app.component.spec.ts
@@ -17,15 +17,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'sat'`, async(() => {
+  it(`should have as title 'sat-client-app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('sat');
+    expect(app.title).toEqual('sat-client-app');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to sat!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to sat-client-app!');
   }));
 });
--- src/sat-client-app/src/app/app.component.ts
+++ src/sat-client-app/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'sat-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sat';
+  title = 'sat-client-app';
 }
